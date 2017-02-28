99bb90819a0eafe7c781f54d0a181bda9f8ca6c7
jeneser <2867483044@qq.com>
2017-02-28 17:30:29 +0800

    添加 发现好书数据

--- src/views/BookView.vue
+++ src/views/BookView.vue
@@ -12,7 +12,7 @@
         </div>
       </div>
     </scroller>
-    <scroller title="发现好书" type="onlyString"></scroller>
+    <scroller title="发现好书" type="onlyString" :items="tags"></scroller>
 
     <types></types>
     <download-app></download-app>
@@ -31,7 +31,42 @@ export default {
     return {
       novel: [],
       reality: [],
-      travel: []
+      travel: [],
+      tags: [
+        {
+          title: '不可饶恕的女人们',
+          href: 'https://m.douban.com/doulist/35573',
+          color: '#42BD56'
+        },
+        {
+          title: '爱欲书',
+          href: 'https://m.douban.com/doulist/38088147',
+          color: '#FF4055'
+        },
+        {
+          title: '他们还写侦探小说',
+          href: 'https://m.douban.com/doulist/645579',
+          color: '#4F9DED'
+        },
+        {
+          line: true
+        },
+        {
+          title: '人生识字始忧患',
+          href: 'https://m.douban.com/doulist/192653',
+          color: '#CC3344'
+        },
+        {
+          title: '詩歌書店',
+          href: 'https://m.douban.com/doulist/89925',
+          color: '#FFAC2D'
+        },
+        {
+          title: '尝试理解人类变化无常不可测心理相关小荐',
+          href: 'https://m.douban.com/doulist/45361809',
+          color: '#3BA94D'
+        }
+      ]
     }
   },
   beforeMount () {
