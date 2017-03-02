7fe091bf6134a5e406ac7b4c0090342b8fd42084
jeneser <2867483044@qq.com>
2017-03-02 22:22:04 +0800

    add groupview

--- src/components/Group.vue
+++ src/components/Group.vue
@@ -1,12 +1,57 @@
 <template>
   <div class="group">
-    group
+    <div class="header">
+      <h2>{{title}}</h2>
+    </div>
+    <ul class="content">
+      <li>
+        <a href="#">
+          <div class="group-meta">
+            <img src="https://img3.doubanio.com/icon/g173252-1.jpg" alt="">
+            <span class="group-name">浦东租房专区</span>
+            <span class="group-member">20005人</span>
+          </div>
+          <div class="group-topic">
+            <span>5分钟前更新：</span>
+            <span>浦东 2号线/7号线 次卧  地铁5分钟</span>
+          </div>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <div class="group-meta">
+            <img src="https://img3.doubanio.com/icon/g173252-1.jpg" alt="">
+            <span class="group-name">浦东租房专区</span>
+            <span class="group-member">20005人</span>
+          </div>
+          <div class="group-topic">
+            <span>5分钟前更新：</span>
+            <span>浦东 2号线/7号线 次卧  地铁5分钟</span>
+          </div>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <div class="group-meta">
+            <img src="https://img3.doubanio.com/icon/g173252-1.jpg" alt="">
+            <span class="group-name">浦东租房专区</span>
+            <span class="group-member">20005人</span>
+          </div>
+          <div class="group-topic">
+            <span>5分钟前更新：</span>
+            <span>浦东 2号线/7号线 次卧  地铁5分钟</span>
+          </div>
+        </a>
+      </li>
+    </ul>
+    <slot name="more"></slot>
   </div>
 </template>
 
 <script>
   export default {
     name: 'group',
+    props: ['title'],
     data () {
       return {
 
@@ -16,5 +61,50 @@
 </script>
 
 <style lang="scss" scoped>
+.group {
+  margin-left: 18px;
+  padding-top: 14px;
+}
+.content {
+  position: relative;
 
+  a {
+    display: block;
+    padding: 15px 18px 15px 0;
+  }
+  .group-meta {
+    display: flex;
+    align-items: center;
+  }
+  .group-name {
+    flex: 1;
+    padding: 0 10px;
+    font-size: 18px;
+    color: #111;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .group-member {
+    color: #ccc;
+    font-size: 15px;
+  }
+  .group-topic {
+    margin-top: 10px;
+    font-size: 15px;
+    color: #aaa;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+}
+li::after {
+  content: '';
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 1px;
+  background: #E8E8E8;
+}
 </style>
--- src/views/GroupView.vue
+++ src/views/GroupView.vue
@@ -1,20 +1,44 @@
 <template>
   <div class="group-view has-header">
-    group
+    <group title="租房找室友">
+      <a class="list-link" href="#" slot="more">更多相关小组</a>
+    </group>
+    <group title="租房找室友">
+      <a class="list-link" href="#" slot="more">来聊五块钱</a>
+    </group>
+    <group title="租房找室友">
+      <a class="list-link" href="#" slot="more">更多相关小组</a>
+    </group>
+    <group title="租房找室友">
+      <a class="list-link" href="#" slot="more">来聊五块钱</a>
+    </group>
+    <download-app></download-app>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'group-view',
-    data () {
-      return {
+import Group from '../components/Group.vue'
+import DownloadApp from '../components/DownloadApp.vue'
 
-      }
+export default {
+  name: 'group-view',
+  components: { Group, DownloadApp },
+  data () {
+    return {
+
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
-
+.list-link {
+  display: block;
+  margin-bottom: 30px;
+  padding: 15px 0;
+  font-size: 16px;
+  line-height: 18px;
+  text-align: center;
+  color: #42bd56;
+}
 </style>
