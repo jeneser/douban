6e345cf8ba77e25d7dbc6ca4b6d37183bf5009bd
jeneser <jeneserwang@gmail.com>
2017-04-08 17:02:22 +0800

    Use InfiniteLoading

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,20 +2,39 @@
   <div class="home-view has-header">
     <sub-nav quickNav="ok"></sub-nav>
     <list :items="events"></list>
+    <infinite-loading :on-infinite="onInfinite" ref="infiniteLoading"></infinite-loading>
 
   </div>
 </template>
 
 <script>
+import InfiniteLoading from 'vue-infinite-loading'
 import SubNav from '../components/SubNav'
 import List from '../components/List'
 
 export default {
   name: 'home-view',
-  components: { SubNav, List },
+  components: { SubNav, List, InfiniteLoading },
   data () {
     return {
-      events: []
+      events: [],
+      temp: []
+    }
+  },
+  methods: {
+    onInfinite () {
+      setTimeout(() => {
+        this.loadMore()
+        this.events = this.events.concat(this.temp)
+        this.$refs.infiniteLoading.$emit('$InfiniteLoading:loaded')
+      }, 1000)
+    },
+    loadMore () {
+      this.$http.jsonp('https://api.douban.com/v2/event/list?loc=108288&start=5&count=5')
+                .then(res => {
+                  console.log(res.body.events)
+                  this.temp = res.body.events
+                })
     }
   },
   beforeMount () {
